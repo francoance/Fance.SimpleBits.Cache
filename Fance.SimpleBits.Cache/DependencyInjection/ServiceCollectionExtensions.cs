c0b3d0685827c0cb5267b22019dddcfbd927e26b
Franco Ance <franco.ance@gmail.com>
2021-02-13 23:03:11 -0300

    Docs: Added comments for all the public members.

--- Fance.SimpleBits.Cache/DependencyInjection/ServiceCollectionExtensions.cs
+++ Fance.SimpleBits.Cache/DependencyInjection/ServiceCollectionExtensions.cs
@@ -5,9 +5,20 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Fance.SimpleBits.Cache.DependencyInjection
 {
+    /// <summary>
+    /// Extension methods for the service collection.
+    /// Used to add services to the dependency injector container.
+    /// </summary>
     public static class ServiceCollectionExtensions
     {
-        public static void AddSimpleCache(this IServiceCollection services, IConfiguration configuration)
+        /// <summary>
+        /// Add the simple redis cache.
+        /// Configures the StackExchangeRedisCache library from Microsoft nuget package.
+        /// Registers ISimpleCache using the SimpleRedisCache type.
+        /// </summary>
+        /// <param name="services">Service collector.</param>
+        /// <param name="configuration">Configuration object.</param>
+        public static void AddSimpleRedisCache(this IServiceCollection services, IConfiguration configuration)
         {
             var config = configuration.GetSection("Cache");
             services.AddStackExchangeRedisCache(setup =>
--- Fance.SimpleBits.Cache/Service/Interfaces/ISimpleCache.cs
+++ Fance.SimpleBits.Cache/Service/Interfaces/ISimpleCache.cs
@@ -2,11 +2,37 @@
 
 namespace Fance.SimpleBits.Cache.Service.Interfaces
 {
+    /// <summary>
+    /// Simple Cache interface.
+    /// </summary>
     public interface ISimpleCache
     {
+        /// <summary>
+        /// Find a string by its key.
+        /// </summary>
+        /// <param name="key">Object's key.</param>
+        /// <returns>String found for the key.</returns>
         string Find(string key);
+        
+        /// <summary>
+        /// Save an object with a key.
+        /// </summary>
+        /// <param name="key">Key for the object.</param>
+        /// <param name="value">String value object.</param>
         void Save(string key, string value);
+        
+        /// <summary>
+        /// Find a string by its key, asynchronically.
+        /// </summary>
+        /// <param name="key">Object's key.</param>
+        /// <returns>String found for the key.</returns>
         Task<string> FindAsync(string key);
+        
+        /// <summary>
+        /// Save an object with a key, asynchronically.
+        /// </summary>
+        /// <param name="key">Key for the object.</param>
+        /// <param name="value">String value object.</param>
         Task SaveAsync(string key, string value);
     }
 }
